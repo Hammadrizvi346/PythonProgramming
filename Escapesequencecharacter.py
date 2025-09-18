a2261f04425970e96b304f44b70524cb51ac50f1
Hammad Rizvi <Hammadrizvi346@gmail.com>
2025-09-19 02:26:05 +0530

    escape sequence character

---
+++ Escapesequencecharacter.py
@@ -0,0 +1,10 @@
+# Here we use escape sequence character to print double quotes in the string
+print("Hey i am a \"good boy\" and i study in btech 12")
+
+#Here we use escape sequence character to print new line in the string
+
+print("Hello ", end="346\n")
+print("World")
+
+# Here we use sep parameter to change the default space separator to hyphen
+print("Hey", 6, 7, sep="-")
---
+++ SecondProgram.py
@@ -0,0 +1,9 @@
+'''
+Comments in Python
+This is a simple Python program
+This line is a comments and not executed
+'''
+print("Hello, World!")  # This line prints a message to the console
+print(5 + 3)  # This line prints the result of an addition operation)
+
+ 
