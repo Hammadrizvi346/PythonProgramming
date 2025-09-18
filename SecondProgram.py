40f29b226ea8ac49b0bf8e35c670f60572e634f4
Hammad Rizvi <Hammadrizvi346@gmail.com>
2025-09-19 02:29:35 +0530

    Comments in Python

--- SecondProgram.py
+++ SecondProgram.py
@@ -1,8 +1,7 @@
-'''
-Comments in Python
-This is a simple Python program
-This line is a comments and not executed
-'''
+# Comments in Python
+# This is a simple Python program
+# This line is a comments and not executed
+
 print("Hello, World!")  # This line prints a message to the console
 print(5 + 3)  # This line prints the result of an addition operation)
 
