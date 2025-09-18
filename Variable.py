78ffa42bf61acd39da1c649e0e79aa7c3c2bf105
Hammad Rizvi <Hammadrizvi346@gmail.com>
2025-09-19 02:42:59 +0530

    variables and Data type in python and how to use them

---
+++ Variable.py
@@ -0,0 +1,27 @@
+#Video 6
+# variables and Data type in python and how to use them
+a=5
+b=6 # integer data type
+c=5.6 # float data type 
+d="Hammad Rizvi" # string data type
+e=True # boolean data type
+f=None # None data type
+g=complex(3,5) # complex data type
+# printing the variables and their data types
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+# printing the sum of two variables
+# we can change the value of variable at any time
+print(a+b)
