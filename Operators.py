013622c8dbefe352f1cc29d279d5da6c5536406e
Hammad Rizvi <Hammadrizvi346@gmail.com>
2025-09-20 00:47:53 +0530

    Operators in Python

---
+++ Operators.py
@@ -0,0 +1,15 @@
+# #Operators in Python are used to perform operations on variables and values.
+print(5+6)
+print(5-6)
+print(5*6)
+print(5/6)
+print(5%6)  
+
+
+#Calculator for addition, subtraction, multiplication, and division for two numbers (numbers is given)
+num1=12
+num2=54
+print("Addition is:",num1+num2)
+print("Subtraction is:",num1-num2) 
+print("Multiplication is:",num1*num2) 
+print("Division is:",num1%num2) 
